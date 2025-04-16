45cb47df53c11d8cfbb604d9ba033184bb65a233
anurajthakur333 <anurajthakur141@gmail.com>
2025-04-17 02:05:51 +0530

    vite base back

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://anuraj.online/',  // Keep the base path for the project
   css: {
     preprocessorOptions: {
       scss: {
